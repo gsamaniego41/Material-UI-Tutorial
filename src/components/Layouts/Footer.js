6030d367be036a49a8d1bccb486b6d1adf657cea
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-03-18 12:45:01 -0700

    Footer: Paper, Tabs, Tab

--- src/components/Layouts/Footer.js
+++ src/components/Layouts/Footer.js
@@ -1,7 +1,18 @@
 import React from "react";
+import {Paper, Tabs, Tab} from "@material-ui/core";
 
 export default props => (
-  <div>
-    <h1>Footer</h1>
-  </div>
+  <Paper>
+    {/* Paper is just a container w/ light border and box-shadow */}
+    <Tabs
+      value={0} // value takes in an index, functions like selected tab
+      indicatorColor="primary"
+      textColor="primary"
+      centered
+    >
+      <Tab label="Item 1" />
+      <Tab label="Item 2" />
+      <Tab label="Item 3" />
+    </Tabs>
+  </Paper>
 );
