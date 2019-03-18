28315b6ec5d31bbee9a430264ae8112c10ff7c83
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-03-18 12:56:43 -0700

    #2: Material Grid Basics

--- src/components/Exercises/index.js
+++ src/components/Exercises/index.js
@@ -1,3 +1,23 @@
 import React from "react";
+import {Grid, Paper} from "@material-ui/core";
+// App biz logic
 
-export default props => <div>Hello from Exercises</div>;
+export default props => (
+  // Grid (2 types) either has to be a grid container or a grid item
+  // 5 Grid breakpoints (12 column system)
+  // xs = 0px or larger
+  // sm = 600px or larger
+  // md = 960px or larger
+  // lg = 1280px or larger
+  // xl = 1920px or larger
+  <Grid container sm={12}>
+    {/* can be customized sm={12} */}
+    <Grid item sm={8}>
+      {/* if sm is not customized, the grid will automatically adjust if we add more items */}
+      <Paper>Left Pane</Paper>
+    </Grid>
+    <Grid item sm={4}>
+      <Paper>Right Pane</Paper>
+    </Grid>
+  </Grid>
+);
