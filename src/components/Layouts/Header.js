8f2248caacead0f65694031edfaf4cd685b9aeac
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-03-18 12:37:52 -0700

    Header: AppBar, Toolbar, Typography

--- src/components/Layouts/Header.js
+++ src/components/Layouts/Header.js
@@ -1,7 +1,19 @@
 import React from "react";
+import {AppBar, Toolbar, Typography} from "@material-ui/core/";
 
 export default props => (
-  <div>
-    <h1>Header</h1>
-  </div>
+  <AppBar position="static">
+    <Toolbar>
+      {/* Custom font sizes
+        display 1-4, headline, title, subheading, body, caption, button 
+        Ex:
+          <Typography variant="display4" gutterBottom>Display 4</Typography>
+
+        gutterBottom = like padding-bottom
+      */}
+      <Typography variant="headline" color="inherit">
+        Exercise Database
+      </Typography>
+    </Toolbar>
+  </AppBar>
 );
