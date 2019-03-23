529589dd676beb0f01ec1c81fd750a404d4db34d
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-03-23 09:47:19 -0700

    #6: modal form, text fields

--- README.md
+++ README.md
@@ -9,3 +9,19 @@
 ~~#5 Dialogs & Icons~~
 
 #6 Forms, Inputs, and Styling
+
+#7 Lists & IconButtons
+
+#8 Forms (Part 1)
+
+#9 Forms (Part 2)
+
+#10 CSS-in-JS
+
+#11 Styling with JS
+
+#12 Theming (Part 1)
+
+#13 Theming (Part 2)
+
+#14 Theming (Part 3)
--- src/components/Exercises/Dialogs/Create.js
+++ src/components/Exercises/Dialogs/Create.js
@@ -1,35 +1,56 @@
 import React, {Component} from "react";
 import {
+  Button,
   Dialog,
-  DialogTitle,
+  DialogActions,
   DialogContent,
   DialogContentText,
-  DialogActions,
-  Button
+  DialogTitle,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField
 } from "@material-ui/core";
 import {Add} from "@material-ui/icons";
 
 export default class extends Component {
-  state = {open: false};
+  state = {
+    open: false,
+    exercise: {
+      title: "",
+      description: "",
+      muscles: ""
+    }
+  };
 
-  handleToggle = () => {
+  handleToggle = () => this.setState({open: true});
+
+  handleClose = () => this.setState({open: false});
+
+  handleChange = name => ({target: {value}}) => {
     this.setState({
-      open: true
+      exercise: {
+        ...this.state.exercise,
+        [name]: value
+      }
     });
   };
 
-  handleClose = () => {
-    this.setState({open: false});
-  };
-
   render() {
-    const {open} = this.state;
+    const {
+        open,
+        exercise: {title, description, muscles}
+      } = this.state,
+      {muscles: categories} = this.props; // from App.js => Header.js => Create.js
+
     return (
       <>
         <Button variant="fab" mini onClick={this.handleToggle}>
           <Add />
         </Button>
 
+        {/* MODAL */}
         <Dialog open={open} onClose={this.handleClose}>
           <DialogTitle id="form-dialog-title">
             Create a New Exercise
@@ -38,6 +59,31 @@ export default class extends Component {
             <DialogContentText>
               Please fill out the form below
             </DialogContentText>
+
+            {/* TEXTFIELDS */}
+            <form>
+              <TextField
+                label="Title"
+                value={title}
+                onChange={this.handleChange("title")}
+                margin="normal"
+              />
+              <br />
+              <FormControl>
+                <InputLabel htmlFor="muscles">Muscles</InputLabel>
+                {/* React Docs - Since for is a reserved word in JavaScript, React elements use htmlFor instead. */}
+              </FormControl>
+              <br />
+              <TextField
+                multiline // expands field if
+                rows="4" // overflow scroll, field fixed at 4 rows
+                label="Description"
+                value={description}
+                onChange={this.handleChange("description")}
+                margin="normal"
+              />
+              <br />
+            </form>
           </DialogContent>
           <DialogActions>
             <Button color="primary" variant="raised">
