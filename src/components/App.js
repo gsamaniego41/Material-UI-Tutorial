79a07c5e4650be6204949ee93ce579a4a35526c1
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-03-24 23:59:18 -0700

    #7: persists muscle categories in left pane

--- src/components/App.js
+++ src/components/App.js
@@ -7,29 +7,25 @@ export default class extends Component {
   state = {exercises, category: "", exercise: {}};
 
   getExercisesByMuscleGroup() {
+    const initExercises = muscles.reduce(
+      (exercises, category) => ({
+        // creates an object w/ a property of the category
+        // and a value of an empty array
+        ...exercises,
+        [category]: []
+      }),
+      {}
+    );
+
+    console.log(muscles, initExercises);
+
     return Object.entries(
-      // Object.entries turns result into an array
-      // w/o Object.entries, reduce will return an object of muscle categories
-      // that has a value of an array of exercises.
-
       this.state.exercises.reduce((exercises, exercise) => {
-        // exercises = accumulator
-        // exercise = item being iterated over
-
         const {muscles} = exercise;
 
-        // counts instances of values in an object
-        // exercises[muscles] creates a muscles property in the accumulator obj
-        exercises[muscles] = exercises[muscles]
-          ? // Do we already have something stored in that muscles property?
-            // in other words, does that muscle category already exists?
-            [...exercises[muscles], exercise]
-          : // if it does, add the current exercise obj to the array of exercises
-            [exercise]; // if it doesn't, create a new category and add this exercise to that category
-
-        // finally, return the array of muscle categories
+        exercises[muscles] = [...exercises[muscles], exercise];
         return exercises;
-      }, {})
+      }, initExercises)
     );
   }
 
